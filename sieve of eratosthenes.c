907be382b0c6e199959f2fd57c21f026af65362a
Marwan Emad <66377535+MarwanEmadMourad@users.noreply.github.com>
2021-01-09 00:17:30 +0200

    Add files via upload

---
+++ sieve of eratosthenes.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h> 
+
+// definig the range to count the prime numbers in
+#define N 100
+
+// global array to be set of prime numbers
+int arr[N] ;
+
+// function to catch prime numbers in a certain range
+void Catch_Prime (int *arr){
+    
+    // there 's no prime-even number except 2 
+    // so setting an array with only odd values starting with 3
+    // here we are assuming that all numbers are prime
+    for (int i = 3 ; i < N ; i+=2)
+    {
+        arr[i] = i ;
+    }
+    
+    // hard coding the first 3 elements of the array 
+    arr[0] = 0 ;
+    arr[1] = 0 ;
+    arr[2] = 2 ;
+    
+    // this loop is responsible for replacing the multiples of the prime with 0 
+    // 0 --> refers to not a prime number
+    // output array of this loop will hold only prime numbers and the rest is 0
+    for (int i = 2 ; i < N ; i++ )
+    {
+        for(int j = 2 ; (i*j) < N ; j++)
+            arr[i*j] = 0 ;
+    }
+	
+  }
+
+
+int main (){
+    
+	 Catch_Prime(arr) ;
+	 int Prime_counter = 0 ;
+	
+	    for (int i = 0 ; i < N ; i++)
+	      {
+	        if(arr[i])
+	        {
+	           printf("%d \n",i) ;
+	           Prime_counter ++ ;
+	        }
+	      }
+	      printf("%d \n" ,Prime_counter) ;
+    
+}
+
+
+
